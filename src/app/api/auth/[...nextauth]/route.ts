a6c8022ce950a060ebf125f5acae972497cf42f2
Daniel <daniel.40.pompa@gmail.com>
2024-12-10 23:54:08 +0100

    feat: add credentials provider for authentication
    - Added `CredentialsProvider` to NextAuth for email/password login.
    - Implemented `signInWithCredentials` function for user authentication.
    - Added logic to create a new user if not found in the database.
    - Updated `authorize` method in `CredentialsProvider` to handle custom login.
    - Ensured roles and ID are included in the session and JWT token.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,9 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { Adapter } from 'next-auth/adapters';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import prisma from '@/lib/prisma';
+import { signInWithCredentials } from '@/auth/actions/auth-actions';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -15,6 +17,30 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Email', type: 'email', placeholder: 'user@example.com' },
+        password: { label: 'Password', type: 'password', placeholder: '********' },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInWithCredentials(
+          credentials!.username,
+          credentials!.password
+        );
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
   session: { strategy: 'jwt' },
