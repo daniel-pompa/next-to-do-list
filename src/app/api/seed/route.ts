3d056f6a5134c5ec048262ce78b8ef9c2ebd8221
Daniel <daniel.40.pompa@gmail.com>
2024-12-11 22:45:40 +0100

    feat: enhance database seeding with user and tasks
    Updated database seeding to include user creation and associated tasks for more comprehensive testing and development setup.
    
    - Cleared existing tasks and users before seeding to avoid conflicts.
    - Added a default user with admin role (`John Doe`) and associated tasks.
    - Retained existing task details while associating them with the created user.
    
    This update ensures a fully functional seed script that initializes a user-task relationship for testing and development purposes.

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,45 +3,53 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    // Delete all existing todos
+    // Delete all existing tasks and users before seeding new data
     await prisma.task.deleteMany();
-    // Create new todo items
-   const tasks = await prisma.task.createMany({
-     data: [
-       {
-         title: 'Next.js Fundamentals',
-         description:
-           'Master routing, pages, and rendering in Next.js for efficient development.',
-       },
-       {
-         title: 'TypeScript Proficiency',
-         description:
-           'Enhance your coding skills with strong typing, interfaces, and classes.',
-         complete: true,
-       },
-       {
-         title: 'UI Design with Tailwind CSS',
-         description:
-           'Build responsive, mobile-first user interfaces using Tailwind CSS.',
-       },
-       {
-         title: 'Database Managemetn with Prisma',
-         description:
-           'Integrate Prisma ORM for robust and type-safe database operations.',
-       },
-       {
-         title: 'PostgreSQL with Docker',
-         description:
-           'Set up and run PostgreSQL databases using Docker for local development.',
-       },
-       {
-         title: 'Full-Stack Development',
-         description:
-           'Build a full-stack application connecting Next.js with a PostgreSQL database.',
-       },
-     ],
-   });
-    console.log(tasks);
+    await prisma.user.deleteMany();
+
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'J0hnD03!',
+        roles: ['admin'],
+        tasks: {
+          create: [
+            {
+              title: 'Next.js Fundamentals',
+              description:
+                'Master routing, pages, and rendering in Next.js for efficient development.',
+            },
+            {
+              title: 'TypeScript Proficiency',
+              description:
+                'Enhance your coding skills with strong typing, interfaces, and classes.',
+              complete: true,
+            },
+            {
+              title: 'UI Design with Tailwind CSS',
+              description:
+                'Build responsive, mobile-first user interfaces using Tailwind CSS.',
+            },
+            {
+              title: 'Database Managemetn with Prisma',
+              description:
+                'Integrate Prisma ORM for robust and type-safe database operations.',
+            },
+            {
+              title: 'PostgreSQL with Docker',
+              description:
+                'Set up and run PostgreSQL databases using Docker for local development.',
+            },
+            {
+              title: 'Full-Stack Development',
+              description:
+                'Build a full-stack application connecting Next.js with a PostgreSQL database.',
+            },
+          ],
+        },
+      },
+    });
     return NextResponse.json({ message: 'Seed executed successfully' });
   } catch (error: unknown) {
     if (error instanceof Error) {
